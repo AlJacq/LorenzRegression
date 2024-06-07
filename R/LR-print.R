a19ee2016da1391276d07f52bffe85480e051b84
alexjacq <alexandre.jacquemain@uclouvain.be>
2024-06-07 13:38:49 +0200

    print method

--- R/LR-print.R
+++ R/LR-print.R
@@ -1,11 +1,12 @@
 #' Printing method for the Lorenz Regression
 #'
-#' \code{print.LR} prints the arguments and estimated coefficients of an object of class \code{LR}.
+#' \code{print.LR} prints the arguments, explained Gini coefficient and estimated coefficients of an object of class \code{"LR"}.
 #'
-#' @param x Output of a call to \code{\link{Lorenz.Reg}}, where \code{penalty="none"}.
+#' @param x An object of class \code{"LR"}.
+#' @param digits The number of significant digits to be passed.
 #' @param ... Additional arguments.
 #'
-#' @return No return value, called for printing an object of class \code{LR} to the console
+#' @return No return value, called for printing an object of class \code{"LR"} to the console
 #'
 #' @seealso \code{\link{Lorenz.Reg}}
 #'
@@ -14,18 +15,18 @@
 #' NPLR <- Lorenz.Reg(Income ~ ., data = Data.Incomes, penalty = "none")
 #' print(NPLR)
 #'
-#' @import knitr
-#'
 #' @method print LR
 #' @export
 
-print.LR <- function(x, ...){
+print.LR <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
-  cat("Call",
-      x$call,
-      sep="\n",
-      "",
-      "Coefficients",
-      knitr::kable(x$theta))
+  if (!inherits(x, "LR")) stop("The object must be of class 'LR'")
+
+  cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+      "\n\n", sep = "")
+  cat("Explained Gini coefficient:", sprintf(paste0("%.", digits, "f"), x$Gi.expl), "\n")
+  cat("\nCoefficients:\n")
+  print.default(format(coef(x), digits = digits), print.gap = 2L,
+                quote = FALSE)
 
 }
--- R/PLR-print.R
+++ R/PLR-print.R
@@ -1,11 +1,12 @@
 #' Printing method for the Penalized Lorenz Regression
 #'
-#' \code{print.PLR} prints the arguments and estimated coefficients of an object of class \code{PLR}.
+#' \code{print.PLR} prints the arguments, explained Gini coefficient and estimated coefficients of an object of class \code{"PLR"}.
 #'
-#' @param x Output of a call to \code{\link{Lorenz.Reg}}, where \code{penalty!="none"}.
+#' @param x An object of class \code{"PLR"}.
+#' @param digits The number of significant digits to be passed.
 #' @param ... Additional arguments.
 #'
-#' @return No return value, called for printing an object of class \code{PLR} to the console
+#' @return No return value, called for printing an object of class \code{"PLR"} to the console.
 #'
 #' @seealso \code{\link{Lorenz.Reg}}
 #'
@@ -16,18 +17,36 @@
 #'                   eps = 0.01, seed.CV = 123, nfolds = 5)
 #' print(PLR)
 #'
-#' @import knitr
-#'
 #' @method print PLR
 #' @export
 
-print.PLR <- function(x, ...){
+print.PLR <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
-  cat("Call",
-      x$call,
-      sep="\n",
-      "",
-      "Coefficients",
-      knitr::kable(t(x$theta)))
+  if (!inherits(x, "PLR")) stop("The object must be of class 'PLR'")
+
+  cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+      "\n\n", sep = "")
+
+  coef_x <- coef(x)
+
+  if(inherits(x,c("PLR_cv","PLR_boot"))){
+
+    cat("\nSelection methods for tuning and penalty parameter:\n",paste0(names(coef_x),collapse=","),"\n")
+    cat("\nExplained Gini coefficient:\n")
+    print.default(format(x$Gi.expl, digits = digits), print.gap = 2L,
+                  quote = FALSE)
+    for (s in names(coef_x)){
+      cat("\nCoefficients obtained by",s,":\n")
+      print.default(format(coef_x[[s]], digits = digits), print.gap = 2L,
+                    quote = FALSE)
+    }
+  }else{
+
+    cat("Explained Gini coefficient:", sprintf(paste0("%.", digits, "f"), x$Gi.expl), "\n")
+    cat("\nCoefficients:\n")
+    print.default(format(coef(x), digits = digits), print.gap = 2L,
+                  quote = FALSE)
+
+  }
 
 }
