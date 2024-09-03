e8425dde1c86b752c35b3d2c3b8670437727f491
alexjacq <alexandre.jacquemain@uclouvain.be>
2024-09-03 14:43:08 +0200

    Distinction between plot() and autoplot() to match the suggestion proposed in the ggplot2 vignette... for LR

--- R/LR-plot.R
+++ R/LR-plot.R
@@ -1,24 +1,27 @@
 #' Plots for the Lorenz Regression
 #'
-#' \code{plot.LR} provides plots for an object of class \code{"LR"}.
+#' \code{autoplot.LR} generates a plot for an object of class \code{"LR"} and returns it as a \code{ggplot} object.
+#' The \code{plot.LR} method is a wrapper around \code{autoplot.LR} that directly displays the plot,
+#' providing a more familiar interface for users accustomed to base R plotting.
 #'
+#' @aliases plot.LR
 #' @param x An object of class \code{"LR"}.
-#' @param ... Additional arguments passed to function \code{\link{Lorenz.graphs}}.
+#' @param ... Additional arguments passed to \code{\link{Lorenz.graphs}}.
 #'
-#' @return The Lorenz curve of the response and concentration curve of the response with respect to the estimated index. The graph is as an object of class \code{"ggplot"}.
+#' @return \code{autoplot.LR} returns a \code{ggplot} object representing the Lorenz curve of the response and the concentration curve of the response with respect to the estimated index. \code{plot.LR} directly displays this plot.
 #'
 #' @seealso \code{\link{Lorenz.Reg}}
 #'
 #' @examples
 #' ## For examples see example(Lorenz.Reg)
 #'
-#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 ggtitle autoplot
 #' @importFrom stats update.formula
 #'
-#' @method plot LR
+#' @method autoplot LR
 #' @export
 
-plot.LR <- function(x, ...){
+autoplot.LR <- function(x, ...){
 
   if (is.null(x$theta)) stop("No plots are available for an empty model.")
 
@@ -33,10 +36,23 @@ plot.LR <- function(x, ...){
 
 }
 
+#' @importFrom graphics plot
+#' @method plot LR
+#' @rdname autoplot.LR
+#' @export
+plot.LR <- function(x, ...) {
+  print(autoplot(x, ...))
+}
+
+#' @method autoplot LR_boot
+#' @export
+autoplot.LR_boot <- function(x, ...){
+  NextMethod("autoplot")
+}
+
 #' @method plot LR_boot
 #' @export
-
-plot.LR_boot <- function(x, ...){
-  NextMethod("plot")
+plot.LR_boot <- function(x, ...) {
+  print(autoplot(x, ...))
 }
 
