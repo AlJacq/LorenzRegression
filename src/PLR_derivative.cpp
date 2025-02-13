0eb551981fa80dc33362972b86382cff0f314055
alexjacq <alexandre.jacquemain@uclouvain.be>
2025-02-13 14:38:06 +0100

    #17 externalized from j-loop in loss and derivative

--- src/PLR_derivative.cpp
+++ src/PLR_derivative.cpp
@@ -20,25 +20,32 @@ arma::vec PLR_derivative_cpp(arma::vec y, arma::mat X, arma::vec pi, arma::vec t
 
   for (i=1; i<n; i++)
   {
+
+    double y_i = y(i);
+    double pi_i = pi(i);
+    double index_i = index(i);
+    arma::rowvec X_i = X.row(i);
+
     for (j=0; j<i; j++)
     {
+      // Avoid computation if increment to der(k) is 0 anyway
+      double a = y_i - y(j);
+      if(a == 0) continue;
 
-      if (j != i)
-      {
-        u =  (index(i) - index(j))/h;
+      u =  (index_i - index(j))/h;
+      // Avoid computation if increment to der(k) is 0 anyway
+      if (u < -1 || u > 1) continue;
 
-        for (k=0; k<p; k++)
-          v(k) =  (X(i,k) - X(j,k))/h;
+      for (k=0; k<p; k++)
+        v(k) =  (X_i(k) - X(j,k))/h;
 
-        if (u < -1 || u > 1) a0=0;
-        if (u >= -1 && u <=1 && kernel == 1) a0 = 9.0/8.0 - 15.0/8.0*pow(u,2.0);
-        if (u >= -1 && u <=1 && kernel == 2) a0 = 45.0/32.0 - 75.0/16.0*pow(u,2.0) + 105.0/32.0*pow(u,4.0);
+      if (u >= -1 && u <=1 && kernel == 1) a0 = 9.0/8.0 - 15.0/8.0*pow(u,2.0);
+      if (u >= -1 && u <=1 && kernel == 2) a0 = 45.0/32.0 - 75.0/16.0*pow(u,2.0) + 105.0/32.0*pow(u,4.0);
 
-        for (k=0; k<p; k++){
-          der(k) = der(k) + 1.0 * pi(i)*pi(j)*(y(i)-y(j)) * a0 * (v(k));
-        }
-
+      for (k=0; k<p; k++){
+        der(k) = der(k) + 1.0 * pi_i*pi(j)*a * a0 * (v(k));
       }
+
     }
   }
 
--- src/PLR_loss.cpp
+++ src/PLR_loss.cpp
@@ -17,12 +17,19 @@ double PLR_loss_cpp(arma::mat X, arma::vec y, arma::vec pi, arma::vec theta, dou
 
   for (i=1; i<n; i++)
   {
+    double y_i = y(i);
+    double pi_i = pi(i);
+    double index_i = index(i);
+
     for (j=0; j<i; j++)
     {
-      u = (index(i)-index(j))/h;
-      if (u > -1 && u < 1 && kernel == 1) sum =  sum + 1.0* pi(i)*pi(j)*(y(i)-y(j)) * (9.0/8.0*u - 5.0/8.0*pow(u,3.0) + 0.5);
-      if (u > -1 && u < 1 && kernel == 2) sum =  sum + 1.0* pi(i)*pi(j)*(y(i)-y(j)) * (45.0/32.0*u - 25.0/16.0*pow(u,3.0) + 21.0/32.0*pow(u,5.0) + 0.5);
-      if (u >= 1) sum =  sum + 1.0* pi(i)*pi(j)*(y(i)-y(j));
+      double a = y_i - y(j);
+      // Avoid computation if increment of sum is 0 anyway
+      if (a == 0) continue;
+      u = (index_i-index(j))/h;
+      if (u > -1 && u < 1 && kernel == 1) sum =  sum + 1.0* pi_i*pi(j)*a * (9.0/8.0*u - 5.0/8.0*pow(u,3.0) + 0.5);
+      if (u > -1 && u < 1 && kernel == 2) sum =  sum + 1.0* pi_i*pi(j)*a * (45.0/32.0*u - 25.0/16.0*pow(u,3.0) + 21.0/32.0*pow(u,5.0) + 0.5);
+      if (u >= 1) sum =  sum + 1.0* pi_i*pi(j)*a;
     }
   }
 
